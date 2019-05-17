32701d1706ab906e99dd613f6c864e19b5038ff3
Marcel Hernandez <e6990620@gmail.com>
2019-05-17 20:09:18 +0200

    Equivalent CombGenerator implementation to that of PHP

--- Uma.Uuid/CombGenerator.cs
+++ Uma.Uuid/CombGenerator.cs
@@ -5,14 +5,18 @@ namespace Uma.Uuid
 {
     public class CombGenerator : IUuidGenerator
     {
+        private const long UnixEpoch = 621355968000000000;
+
         private readonly IUuidGenerator _version4 = new Version4Generator();
 
         public Uuid NewUuid(string name = null)
         {
+            var tmp = new byte[8];
+            var timestamp = DateTimeOffset.Now.Ticks - UnixEpoch;
+            BinaryPrimitives.WriteInt64BigEndian(tmp.AsSpan(), (timestamp / 10) << 4);
+
             var bytes = new byte[16];
-
-            BinaryPrimitives.WriteUInt64BigEndian(bytes.AsSpan(), (ulong) DateTimeOffset.Now.Ticks);
-
+            Array.Copy(tmp, 1, bytes, 0, 6);
             Array.Copy(_version4.NewUuid().ToByteArray(), 6, bytes, 6, 10);
 
             return new Uuid(bytes);
