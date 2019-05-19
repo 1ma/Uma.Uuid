2723a6cf5881b2126dd9a70d4fdc9d0805a5fa04
Marcel Hernandez <e6990620@gmail.com>
2019-05-19 16:49:14 +0200

    test divergence between Guid and Uuid serialization

--- Uma.Uuid.Tests/UuidTest.cs
+++ Uma.Uuid.Tests/UuidTest.cs
@@ -30,6 +30,22 @@ namespace Uma.Uuid.Tests
         }
 
         [Fact]
+        public void TestUuidGuidDivergence()
+        {
+            var bytes = new byte[]
+            {
+                0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
+            };
+
+            var uuid = new Uuid(bytes);
+            var guid = new Guid(bytes);
+
+            Assert.Equal("01234567-89ab-cdef-fedc-ba9876543210", uuid.ToString());
+            Assert.Equal("67452301-ab89-efcd-fedc-ba9876543210", guid.ToString());
+        }
+
+        [Fact]
         public void TestGuidConversion()
         {
             const string text = "00112233-4455-6677-8899-aabbccddeeff";
